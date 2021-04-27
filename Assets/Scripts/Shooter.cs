d01e63149a4e86f982b626c1c26fe95b85d98578
excitexcite <excitenew@gmail.com>
2021-04-27 08:11:26 +0300

    Add defender (cactus), add idle and attack animations. Add Shooter script that allows attacker shoot projectiles.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject gun;
+    [SerializeField] float damage;
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+
+}
