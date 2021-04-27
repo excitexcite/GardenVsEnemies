4dc4876dbeca8978f3fa50bf11c5eb59b7e7985a
excitexcite <excitenew@gmail.com>
2021-04-27 08:45:04 +0300

    Add projectile (zucchini) animation. Implement ability to damage attacker by defenders with projectiles.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] int damage = 100;
+
+    public int GetDamage() { return damage; }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DealDamage(damage);
+        DestroyProjectile();
+    }
+
+    private void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+}
