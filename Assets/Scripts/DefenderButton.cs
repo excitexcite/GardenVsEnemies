066493ee10e88dea8585b41eaffb0685b9f11b30
excitexcite <excitenew@gmail.com>
2021-04-28 03:04:03 +0300

    Add buttons row that allows to select defender to place. Implement ability to change color on defender selection.

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is used to change color (look like they become active)
+/// on defenders exaples (buttons) when player click on them. Placed on each defender button
+/// </summary>
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        // iterate through all buttons for defender creation
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+        // make selected one white (active)
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
