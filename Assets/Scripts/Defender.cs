992735cf8390fe407a53a55d3e2e35c4df96e52a
excitexcite <excitenew@gmail.com>
2021-04-28 06:32:14 +0300

    Add ability to earn resources. Add earn resources animation.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -8,4 +8,11 @@ public class Defender : MonoBehaviour
 
     public int GetStarCost() { return starCost; }
 
+    // this method is for trophy, it is called as animation event, it allows us earn resources
+    public void AddStars(int amount)
+    {
+        // getting StarsDisplay object, that stores amount of stars and add start to total amount
+        FindObjectOfType<StarsDisplay>().AddStars(amount);
+    }
+
 }
