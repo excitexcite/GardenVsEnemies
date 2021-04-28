b687c3376e37f78db694764265f023e6b428498d
excitexcite <excitenew@gmail.com>
2021-04-28 05:26:03 +0300

    Add star display script that allows to store the amount of available resources (stars) and allows to spend/earn stars

---
+++ Assets/Scripts/StarsDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// This script is used to show amount of available start. It has functions to increase amount of start on stars earn and 
+/// decrease stars when used buys a defender.
+/// Placed on startText TMP in canvas.
+/// </summary>
+public class StarsDisplay : MonoBehaviour
+{
+
+    [SerializeField] int stars = 100;
+    [SerializeField] TextMeshProUGUI starsText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        starsText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount) { stars += amount; }
+
+    public void SpendStars(int amount)
+    {
+        // if we have enought stars to buy a defender
+        if (stars >= amount)
+        {
+            stars -= amount;
+        }
+    }
+
+}
