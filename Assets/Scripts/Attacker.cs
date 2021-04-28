ac889a513bde23a011a49756d62c963a1de0d619
excitexcite <excitenew@gmail.com>
2021-04-29 02:28:42 +0300

    Implement ability to attack defenders for lizard attacker.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -10,17 +10,43 @@ public class Attacker : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Animator animator;
+    GameObject currentTarget;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime); 
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        UpdateAnimationState(); // checking for the moment when attacker killed the target
+    }
+
+    // fuction that change animation from attack to walk when attacker has killed the target
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    // function that allows attack the defender, this function call is placed in lizard (other other
+    // named scripts) script
+    public void Attack(GameObject target)
+    {
+        animator.SetBool("IsAttacking", true); // starting attack animation
+        currentTarget = target; // setting attack target to the particular defender
+    }
+
+    // fuction that allow to deal damage to defenders
+    public void StrikeCurrentTarget(int damage)
+    {
+        if (!currentTarget) { return; }
+        // getting defender's health component
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage); // dealing damage
+        }
     }
 
     public void SetMovementSpeed(float speed) { moveSpeed = speed; } 
